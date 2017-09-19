5bff1a9bae93066060706f654fbb22b59f37f029
syominsergey <syominsergey@gmail.com>
2017-09-19 20:16:00 +0300

    перенос методов по изменению LongNumber в сам  класс. LongNumber

--- Roi_2013_1/src/No3_SumOfNumbers.java
+++ Roi_2013_1/src/No3_SumOfNumbers.java
@@ -12,7 +12,7 @@ public class No3_SumOfNumbers
         this.working_dir = working_dir;
     }
     
-    public static class LongNumber {
+    public static class LongNumber implements Cloneable {
         byte[] ciphers;
         int start_pos;
 
@@ -27,76 +27,106 @@ public class No3_SumOfNumbers
         {
             return String.format("LongNumber{ciphers=%s, start_pos=%s}", Arrays.toString(ciphers), start_pos);
         }
-    }
-    
-    LongNumber readNumberFromFile(File file) throws IOException {
-        int size = (int) file.length();
-        if(size==0){
-            throw new IOException("входной файл имеет нулевой размер");
+        
+        @Override
+        public Object clone(){
+            byte[] ciphers_clone = ciphers.clone();
+            return new LongNumber(ciphers_clone, this.start_pos);
         }
-        PushbackInputStream in = new PushbackInputStream(new BufferedInputStream(new FileInputStream(file)), 1);
-        try {
-            int nextCipher = -1;
-            while(true){
-                if(size==0){
-                    throw new IllegalArgumentException("во входном файле не обнаружено цифр");
+        
+        public void increment(){
+            for(int i = start_pos; i<ciphers.length; i++){
+                if(ciphers[i]<9){
+                    ciphers[i]++;
+                    return;
+                } else {
+                    ciphers[i] = 0;
                 }
-                nextCipher = in.read();
-                if(nextCipher==-1){
-                    throw new IOException("Преждевременное окончание файла!");
-                }
-                if(nextCipher>'9'){
-                    size--;
-                    continue;
-                }
-                if(nextCipher<'0'){
-                    size--;
-                    continue;
-                }
-                in.unread(nextCipher);
-                break;
             }
-            byte[] number = new byte[size];
-            int start_pos;
-            for(start_pos = size-1; start_pos>=0; start_pos--){
-                nextCipher = in.read();
-                if(nextCipher==-1){
-                    throw new IOException("Преждевременное окончание файла!");
+        }
+        
+        void decrement(){
+            for(int i = start_pos; i<ciphers.length; i++){
+                if(ciphers[i]>0){
+                    ciphers[i]--;
+                    return;
+                } else {
+                    ciphers[i] = 9;
                 }
-                if(nextCipher>'9'){
-                    start_pos++;
+            }
+        }
+        
+        public static LongNumber readNumberFromFile(File file) throws IOException {
+            int size = (int) file.length();
+            if(size==0){
+                throw new IOException("входной файл имеет нулевой размер");
+            }
+            PushbackInputStream in = new PushbackInputStream(new BufferedInputStream(new FileInputStream(file)), 1);
+            try {
+                int nextCipher = -1;
+                while(true){
+                    if(size==0){
+                        throw new IllegalArgumentException("во входном файле не обнаружено цифр");
+                    }
+                    nextCipher = in.read();
+                    if(nextCipher==-1){
+                        throw new IOException("Преждевременное окончание файла!");
+                    }
+                    if(nextCipher>'9'){
+                        size--;
+                        continue;
+                    }
+                    if(nextCipher<'0'){
+                        size--;
+                        continue;
+                    }
+                    in.unread(nextCipher);
                     break;
                 }
-                if(nextCipher<'0'){
-                    start_pos++;
+                byte[] number = new byte[size];
+                int start_pos;
+                for(start_pos = size-1; start_pos>=0; start_pos--){
+                    nextCipher = in.read();
+                    if(nextCipher==-1){
+                        throw new IOException("Преждевременное окончание файла!");
+                    }
+                    if(nextCipher>'9'){
+                        break;
+                    }
+                    if(nextCipher<'0'){
+                        break;
+                    }
+                    number[start_pos] = (byte) (nextCipher - '0');
+                }
+                start_pos++;
+                return new LongNumber(number, start_pos);
+            } finally {
+                in.close();
+            }
+        }
+        
+        void write(PrintWriter pw) throws IOException {
+            int start_pos = ciphers.length - 1;
+            for(; start_pos>=this.start_pos; start_pos--){
+                if(ciphers[start_pos]>0){
                     break;
                 }
-                number[start_pos] = (byte) (nextCipher - '0');
             }
-            return new LongNumber(number, start_pos);
-        } finally {
-            in.close();
-        }
-    }
-    
-    void incrementNumber(LongNumber number){
-        for(int i = number.start_pos; i<number.ciphers.length; i++){
-            if(number.ciphers[i]<9){
-                number.ciphers[i]++;
+            if(start_pos==this.start_pos-1){
+                pw.print('0');
                 return;
-            } else {
-                number.ciphers[i] = 0;
+            }
+            for(int i = start_pos; i>=this.start_pos; i--){
+                pw.print(ciphers[i]);
             }
         }
-    }
-    
-    void decrementNumber(LongNumber number){
-        for(int i = number.start_pos; i<number.ciphers.length; i++){
-            if(number.ciphers[i]>0){
-                number.ciphers[i]--;
-                return;
-            } else {
-                number.ciphers[i] = 9;
+
+        void write(File file) throws IOException {
+            PrintWriter pw = new PrintWriter(file);
+            try {
+                write(pw);
+            } finally {
+                pw.close();
             }
         }
     }
@@ -128,7 +158,7 @@ public class No3_SumOfNumbers
                 if(!checkNumberIsBeautiful(numberB)){
                     continue;
                 }
-                incrementNumber(answer);
+                answer.increment();
                 if(answer.ciphers[answer.ciphers.length-1] != 1){
                     continue;
                 }
@@ -139,41 +169,20 @@ public class No3_SumOfNumbers
                 answer.ciphers[answer.start_pos] = 0;
             } finally {
                 //изменение чисел a и b или выход из цикла
-                decrementNumber(numberB);
+                numberB.decrement();
                 if(numberB.ciphers[numberB.ciphers.length-1]==0){
                     break;
                 }
-                incrementNumber(numberA);
+                numberA.increment();
             }
         }
         return answer;
     }
     
-    void writeAnswer(LongNumber answer, File file) throws IOException {
-        PrintWriter pw = new PrintWriter(file);
-        try {
-            int start_pos = answer.ciphers.length - 1;
-            for(; start_pos>=answer.start_pos; start_pos--){
-                if(answer.ciphers[start_pos]>0){
-                    break;
-                }
-            }
-            if(start_pos==answer.start_pos-1){
-                pw.print('0');
-                return;
-            }
-            for(int i = start_pos; i>=answer.start_pos; i--){
-                pw.print(answer.ciphers[i]);
-            }
-        } finally {
-            pw.close();
-        }
-    }
-    
     public void run() throws IOException {
-        LongNumber number = readNumberFromFile(new File(working_dir, "aplusb.in"));
+        LongNumber number = LongNumber.readNumberFromFile(new File(working_dir, "aplusb.in"));
         LongNumber answer = computeAnswer(number);
-        writeAnswer(answer, new File(working_dir, "aplusb.out"));
+        answer.write(new File(working_dir, "aplusb.out"));
     }
     
     public static void main(String[] atgs) throws IOException{
